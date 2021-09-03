5caaf55eba26d9e574470b32c5267048a4829a48
ChristopherHusk <40524038+inofearu@users.noreply.github.com>
2021-09-03 19:49:15 +0000

    brain cells failing

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 from colorama import Fore
 from colorama import Style
 from colorama import Back
-from termcolor import colored
 def sleep():
     t = random.randint(0, 5)
     time.sleep(t)
@@ -145,7 +144,7 @@ def secret():
    global encounter_rate_modifier
    encounter_rate_modifier = 10000
    global stat_points
-   stat_points = -20
+   stat_points = 0
 
 
 
@@ -171,30 +170,79 @@ else:
 print()
 print("What difficulty would you like? \n 1 for very easy \n 2 for easy \n 3 for normal \n 4 for hard", Fore.RED,"\n 5 for soul-crushing \n")
 print(Style.RESET_ALL)
-diffnum = str(input())
-if diffnum in ["1", "2", "3", "4", "5", "6"]:
- if diffnum in ["1"]:
-   very_easy()
- if diffnum in ["2"]:
-   easy() 
- if diffnum in ["3"]:
-   normal()
- if diffnum in ["4"]:
-   hard()
- if diffnum in ["5"]:
-   soul_crushing()
- if diffnum in ["6"]:
-   secret()
-   print(Fore.RED)
-   print(Back.BLACK)
-   print('What have you done')
-   print(Style.RESET_ALL)
-else:
-  print("Welp, has to be between 1 and 5, gotta restart")
- 
-  
-
-print('Allocate your stat points!')
-print('You have', stat_points)
-stat_points = int(stat_points)
-strength = int(input())
+def difficulty_setting():
+ global diffnum
+ diffnum = str(input())
+ if diffnum in ["1", "2", "3", "4", "5", "6"]: 
+   diffnum = int(diffnum)
+   if diffnum == 1:
+     very_easy()
+   if diffnum == 2:
+     easy() 
+   if diffnum == 3:
+     normal()
+   if diffnum == 4:
+     hard()
+   if diffnum == 5:
+     soul_crushing()
+   if diffnum == 6:
+     secret()
+     print(Fore.RED)
+     print(Back.BLACK)
+     print('What have you done')
+     print(Style.RESET_ALL)
+ else: 
+   print("This has to be between 1 and 5, please put a new number in")
+   difficulty_setting()
+difficulty_setting()
+def stat_point_assign():
+  global stat_points
+  print ('Allocate your stat points!')
+  print('You have', stat_points)
+  stat_points = int(stat_points)
+  strength = int(input('How many points do you want in strength? \n'))
+  if strength in range(0,10):
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - strength
+  print('You have',stat_points, 'left')
+  perception = int(input('How many points do you want in perception? \n'))
+  if perception in range(0,10):
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - perception
+  print('You have',stat_points, 'left')
+  endurance = int(input('How many points do you want in endurance? \n'))
+  if endurance in range(0,10):
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - endurance
+  print('You have',stat_points, 'left')
+  charisma = int(input('How many points do you want in charisma? \n'))
+  if charisma in range(0,10):
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - charisma
+  print('You have',stat_points, 'left')
+  intelligence = int(input('How many points do you want in intelligence? \n'))
+  if intelligence > 10:
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - intelligence
+  print('You have',stat_points, 'left')
+  agility = int(input('How many points do you want in agility? \n'))
+  if agility > 10:
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - agility
+  print('You have',stat_points, 'left')
+  luck = int(input('How many points do you want in luck? \n'))
+  if luck > 10:
+    print('Please pick a number between 0 and 10')
+    stat_point_assign()
+  stat_points = stat_points - luck
+  if stat_points < 0:
+    print('Error, negative stat points. Please redo.')
+    stat_points = stat_points + strength + perception + endurance + charisma + intelligence + agility + luck
+    stat_point_assign()
+stat_point_assign()
