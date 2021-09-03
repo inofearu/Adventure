2aec8881b2eacd58a2e3223630363965017a313a
ChristopherHusk <40524038+inofearu@users.noreply.github.com>
2021-09-03 19:17:02 +0000

    Actual Initial Commit

--- main.py
+++ main.py
@@ -4,12 +4,148 @@ import time
 from colorama import Fore
 from colorama import Style
 from colorama import Back
-
+from termcolor import colored
 def sleep():
     t = random.randint(0, 5)
     time.sleep(t)
 
 # functions and lists
+
+# Difficultys
+
+# very easy
+def very_easy():
+  #modifiers
+  global enemy_health_modifier
+  enemy_health_modifier = 0.5
+  global loot_quality 
+  loot_quality = 1
+  global loot_amount
+  loot_amount = 1
+  global drop_chance
+  drop_chance = 1
+  global player_health_modifier
+  player_health_modifier = 1.5
+  global gold_loot_amount
+  gold_loot_amount = 2
+  global story_cake_end
+  story_cake_end = 1
+  global encounter_rate_modifier
+  encounter_rate_modifier = 0.8
+  global stat_points
+  stat_points = 50
+
+# easy
+def easy():
+  #modifiers
+  global enemy_health_modifier
+  enemy_health_modifier = 1
+  global loot_quality 
+  loot_quality = 1
+  global loot_amount
+  loot_amount = 1
+  global drop_chance
+  drop_chance = 1
+  global player_health_modifier
+  player_health_modifier = 1
+  global gold_loot_amount
+  gold_loot_amount = 1
+  global story_cake_end
+  story_cake_end = 0
+  global encounter_rate_modifier
+  encounter_rate_modifier = 0.9
+  global stat_points
+  stat_points = 40
+  
+# normal
+def normal():
+  #modifiers
+  global enemy_health_modifier
+  enemy_health_modifier = 1
+  global enemy_damage
+  enemy_damage = 1
+  global loot_quality 
+  loot_quality = 1
+  global loot_amount
+  loot_amount = 1
+  global drop_chance
+  drop_chance = 1
+  global player_health_modifier
+  player_health_modifier = 1
+  global gold_loot_amount
+  gold_loot_amount = 1
+  global story_cake_end
+  story_cake_end = 0
+  global encounter_rate_modifier
+  encounter_rate_modifier = 1
+  global stat_points
+  stat_points = 30
+  
+# hard
+def hard():
+  #modifiers
+  global enemy_health_modifier
+  enemy_health_modifier = 1.2
+  global loot_quality 
+  loot_quality = 1.1
+  global loot_amount
+  loot_amount = 0.75
+  global drop_chance
+  drop_chance = 1
+  global player_health_modifier
+  player_health_modifier = 0.8
+  global gold_loot_amount
+  gold_loot_amount = 0.75
+  global story_cake_end
+  story_cake_end = 0
+  global encounter_rate_modifier
+  encounter_rate_modifier = 1.1
+  global stat_points
+  stat_points = 20
+
+# soul-crushing
+def soul_crushing():
+  
+  #modifiers
+  global enemy_health_modifier
+  enemy_health_modifier = 1
+  global loot_quality  
+  loot_quality = 1.15
+  global loot_amount
+  loot_amount = 1
+  global drop_chance
+  drop_chance = 1
+  global player_health_modifier
+  player_health_modifier = 1
+  global gold_loot_amount
+  gold_loot_amount = 1
+  global story_cake_end
+  story_cake_end = 0
+  global encounter_rate_modifier
+  encounter_rate_modifier = 10
+  global stat_points
+  stat_points = 10
+
+# secret
+def secret():
+   global enemy_health_modifier
+   enemy_health_modifier = 100
+   global loot_quality 
+   loot_quality = 0.00001
+   global loot_amount
+   loot_amount = 0.000001
+   global drop_chance
+   drop_chance = 0.0000001
+   global player_health_modifier
+   player_health_modifier = 0.001
+   global gold_loot_amount
+   gold_loot_amount = 0
+   global story_cake_end
+   story_cake_end = 0
+   global encounter_rate_modifier
+   encounter_rate_modifier = 10000
+   global stat_points
+   stat_points = -20
 
 
 
@@ -17,25 +153,48 @@ def sleep():
 Yes = ["Yes", "yes", "Y", "y", "Sure", "sure", "Ye", "ye"]
 
 # Starting Defines
-Gold = 100
-Health = 100
-Stamina = 100
-Stat_Points = 30
+gold = 100
+health = 100
+stamina = 100
+stat_points = 30
 # Program Start
-name = input("What is your name? \n")
+name = input("What is your name? \n> ")
 if name in ["god","God"]:
-  Gold = 99999
-  Health = 99999
-  Stamina = 99999
-  Stat_Points = 99999
+  gold = 99999
+  phealth = 99999
+  pstamina = 99999
+  stat_points = 99999
 else: 
   if name in ["demigod","Demigod"]:
     Gold = 99999
-    Stat_Points = 99999
+    stat_points = 99999
 print()
-DiffNum = input("What difficulty would you like? \n 1 for very easy \n 2 for easy \n 3 for normal \n 4 for hard \n 5 for soul-crushing \n")
+print("What difficulty would you like? \n 1 for very easy \n 2 for easy \n 3 for normal \n 4 for hard", Fore.RED,"\n 5 for soul-crushing \n")
+print(Style.RESET_ALL)
+diffnum = str(input())
+if diffnum in ["1", "2", "3", "4", "5", "6"]:
+ if diffnum in ["1"]:
+   very_easy()
+ if diffnum in ["2"]:
+   easy() 
+ if diffnum in ["3"]:
+   normal()
+ if diffnum in ["4"]:
+   hard()
+ if diffnum in ["5"]:
+   soul_crushing()
+ if diffnum in ["6"]:
+   secret()
+   print(Fore.RED)
+   print(Back.BLACK)
+   print('What have you done')
+   print(Style.RESET_ALL)
+else:
+  print("Welp, has to be between 1 and 5, gotta restart")
+ 
+  
 
-
-
-
-
+print('Allocate your stat points!')
+print('You have', stat_points)
+stat_points = int(stat_points)
+strength = int(input())
